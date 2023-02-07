4d7efe9e66f9cc2709531f2bc26cf1c1badff8b4
Mohd Azlan Ab Aziz <mhdazlanabaziz@gmail.com>
2023-02-07 16:15:40 +0800

    semakan pertama

---
+++ hello.py
@@ -0,0 +1,19 @@
+#Mohd Azlan Ab Aziz
+#019-2856494
+#azlan@pmu.edu.my
+#name = input("What is your name?\n")
+#greeting = "Hello" + " " + name
+#print(greeting)
+#weigth = float(input("What is your weigth?\n"))
+#height = float(input("What is your heigth?\n"))
+#BMI = weight / (height*height)
+#BMI = weigth/(height*height)
+#print(name + " " + ",your BMI is " + str(BMI))
+
+# Develop a software to calculate Sales
+# 1) Ask name of a user
+# 2) Ask for How many percent is the sales Tax, how much is the price of the goods
+# sales tax = percentage of tax * price amount of good
+# 3)The output: "HI xxxxx  , the total amount to pay is xxx, the sales tax is xxx
+# sales amount = total sales + sales tax
+# barang $1500 tax 6.25%   maka nilai keseluruhan ialah 1631.25"
